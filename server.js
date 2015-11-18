0685bb65e8c25963345cb23bfa3a0e7312104adb
Eslam Mahmoud <contact@eslam.me>
2015-11-18 20:59:52 +0200

    checkpoint all is good now text and video :)

--- server.js
+++ server.js
@@ -3,14 +3,19 @@ var express = require('express');
 //creat web server
 var app = express();
 
+//Create HTTP server 
+var server = require('http').Server(app);
+//Start the real time part in the app
+var io = require('socket.io')(server);
+
 //set the app port
 var port = process.env.PORT || 3000;
 //the room name which user is connected to
 var param_room = null;
-//set the static folder full path
-var staticPath = __dirname+'/static';
-//set the static folder full path
-var htmlPath = __dirname+'/views';
+//set the static/public folder full path
+var staticPath = __dirname + '/static';
+//set the views/html folder full path
+var htmlPath = __dirname + '/views';
 
 //defin the static/public folder
 app.use(express.static(staticPath));
@@ -29,15 +34,45 @@ app.get("/r/:room", function(req, res){
 });
 
 //start the web server on the difiend port
-app.listen(port, function(){
+//MUST BE ON SERVER NOT APP OR SOCKET.IO WILL NOT WORK
+server.listen(port, function(){
 	console.log('web server start on port ' + port);
 });
 
-// //Create HTTP server 
-// var http = require('http').Server(app);
-// //Start the real time part in the app
-// var io = require('socket.io')(http);
-// //listen when new connection happen on socket
-// io.sockets.on('connection', function (socket) {
-// 	console.log('New socket connection id ' + socket.id);
-// });
+//Every socket.io application begins with a connection handler
+io.sockets.on('connection', function (socket) {
+	console.log('New socket connection');
+    //get room name from URL
+    socket.room = param_room;
+    //Add user to the room join/create
+	socket.join(socket.room);
+
+    //Send welcome message to the user(socket) how joined
+    socket.emit('message', { message: 'Welcome to the chat' });
+
+    //Listen when the user send a message
+    socket.on('send', function (data) {
+        //Send the data to all other users inclouding the one sent it
+        io.sockets.in(socket.room).emit('message', {message: data.message, from: socket.username});
+    });
+
+    //Listen when the user send a his username
+    socket.on('username', function (data) {
+        //Send the data to all other users inclouding the one sent it
+        socket.username = data.username;
+    	//Send note to all users in the room
+    	socket.broadcast.to(socket.room).emit('message',{ message: 'New user joined "'+socket.username+'"' });
+    });
+
+    //Send to all users a message because user disconnect
+    socket.on('disconnect', function(){
+		socket.leave(socket.room);
+		socket.broadcast.to(socket.room).emit('message', {message: 'User "'+socket.username+'" disconnected'});
+	});
+
+	//Listen if user is typing
+	socket.on('typing', function (data) {
+        //Send the data to all other users the user name and typing status
+       	socket.broadcast.to(socket.room).emit('typing', {typing: data.typing, username: socket.username});
+    });
+});
--- static/js/room.js
+++ static/js/room.js
@@ -1,16 +1,111 @@
+//Define the socket that we will communicate on
+var socket = false;
+//list of users who are typing now
+var typingList = {};
+// // to stop the app from add the event more than one time
+// var eventsAdded = false;
+
 //Every thing will happen after the Dom loaded
 $(document).ready(function() {
     //Join a room
     $('form#usernameForm').submit( function(e){
         e.preventDefault();
-        //set the layout
+        //connect to the server
+        socket = io().connect();
+        //update the layout
         $('#roomLink').html(window.location.origin+window.location.pathname);
         $('#userName').hide();
         $('#chat').show();
         //Start the WebRTC to get video chat
         init();
+        //Regester the socket events and send username
+        // if(!eventsAdded) {
+        //     eventsAdded = true;
+            events();
+            sendUsername();
+        // }
     });
 });
+
+function sendMessage() {
+    //Send the message to the server
+    var text = $('#message').val();
+    socket.emit('send', { message: text });
+    //Clear the input for the next message
+    message.value = "";
+    sendTyping(false);
+}
+
+function sendUsername() {
+    //Send the username to the server
+    socket.emit('username', { username: $('#name').val() });
+    //Clear the input
+    $('#name').val('');
+}
+
+function events(){
+    //Listem when the server send a message to the user
+    socket.on('message', function (data) {
+        // console.log('Got message', data)
+        if(data.message) {
+            //Render the message and who sent it
+            $('#messages').append($('<p>').html('<b>' + (data.from ? cleanText(data.from) : 'Server') + ': </b>' + cleanText(data.message)));
+            updateScroll();
+        } else {
+            //log in the console if there was not any message
+            console.log("There is a problem:", data);
+        }
+    });
+
+    //Listem to typing status event
+    socket.on('typing', function (data) {
+        //Set the status on users list
+        typingList[data.username] = data.typing;
+        $('#typing').html('');
+        //Loop on all users and get only the ones who are typing
+        var typingUsers = [];
+        for (username in typingList) {
+            if (typingList[username]) {
+                typingUsers.push(username);
+            }
+        }
+        if (typingUsers.length) {
+            $('#typing').html("Typing: ("+ cleanText(typingUsers.join(", ")) +")");                
+        }
+    });
+
+    $("#message").keyup(function(e) {
+        if ($('#message').val()) {
+            sendTyping(true);
+        } else {
+            sendTyping(false);
+        }
+    });
+
+
+    //Send message when the submit button clicked
+    $('form#chatForm').submit( function(e){
+        e.preventDefault();
+        sendMessage();
+        return false;
+    });
+}
+
+//sendTyping function will fire when user type or clrear the input message
+function sendTyping(isUserTyping) {
+    socket.emit('typing', { typing: isUserTyping });
+};
+
+//encode HTML to render it 
+function cleanText(text) {
+    return $('<div/>').text(text).html();
+}
+
+//Keep messages div scrolled to bottom unless user scrolls up
+function updateScroll(){
+    var element = document.getElementById("messages");
+    element.scrollTop = element.scrollHeight;
+}
 
 function init () {
 	var webrtc = new SimpleWebRTC({
